c30c372e4c6e7fe8913d974f4e9c16cbc1e66907
web-text <rehehehj@gmail.com>
2025-06-03 10:03:18 -0700

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyDVnljaBJSqS-xTdI0w-jRNc2Ro5PySeOM",
+    authDomain: "webbtext.firebaseapp.com",
+    projectId: "webbtext",
+    storageBucket: "webbtext.firebasestorage.app",
+    messagingSenderId: "1089670740646",
+    appId: "1:1089670740646:web:d79f4fa6944e41b49d1267",
+    measurementId: "G-EF0V6ZMB0Y"
+  };
+  
